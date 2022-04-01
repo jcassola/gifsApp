f5c705dd36b1d34a8de53ef644525fb106ec1908
Elodin <jcassola@uclv.cu>
2022-03-31 22:31:56 -0400

    gifs modules and components

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class BusquedaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
