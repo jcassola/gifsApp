b9f341fc14c5f099f7bbc0e4a1d4d5d734c05332
Elodin <jcassola@uclv.cu>
2022-05-01 11:25:16 -0400

    make http request

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -12,17 +13,22 @@ export class GifsService {
     return [...this._historial];
   }
 
+  constructor(private http: HttpClient){}
+
   buscarGifs(query:string = ''){
 
     query = query.trim().toLowerCase();
 
     if (!this._historial.includes(query)){
       this._historial.unshift(query);
-
       this._historial = this._historial.splice(0,10);
 
     }
 
-    console.log(this._historial);
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=tUdk085bhue5phmKr7Q3TdjlblMYlKcw&q=One Piece&limit=10')
+          .subscribe((resp: any) => {
+            console.log(resp.data);
+          });
+
   }
 }
